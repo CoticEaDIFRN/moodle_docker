3b213ee82b378f0fff7c1d8934362e20fc11be81
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2018-11-28 16:27:33 -0300

    [UPD] deploy now install databases

--- conf/ava/config.php
+++ conf/ava/config.php
@@ -8,13 +8,13 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = _env('MOODLE_DOCKER_DBTYPE');
+$CFG->dbtype    = _env('MOODLE_DOCKER_DBTYPE', 'mariadb');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = _env('MOODLE_DOCKER_DBHOST');
-$CFG->dbname    = _env('MOODLE_DOCKER_DBNAME');
-$CFG->dbuser    = _env('MOODLE_DOCKER_DBUSER');
-$CFG->dbpass    = _env('MOODLE_DOCKER_DBPASS');
-$CFG->prefix    = 'm_';
+$CFG->dbhost    = _env('MOODLE_DOCKER_DBHOST', 'db');
+$CFG->dbname    = _env('MOODLE_DOCKER_DBNAME', 'ava_base');
+$CFG->dbuser    = _env('MOODLE_DOCKER_DBUSER', 'ava_user');
+$CFG->dbpass    = _env('MOODLE_DOCKER_DBPASS', 'ava_pass');
+$CFG->prefix    = 'mdl_';
 $CFG->dboptions = ['dbcollation' => _env('MOODLE_DOCKER_DBCOLLATION')];
 
 $CFG->wwwroot   = _env('MOODLE_DOCKER_AVA_HOST', 'http://localhost/ava');
@@ -39,47 +39,5 @@ $CFG->passwordpolicy = 0;
 $CFG->phpunit_dataroot  = '/var/www/phpunitdata';
 $CFG->phpunit_prefix = 't_';
 define('TEST_EXTERNAL_FILES_HTTP_URL', 'http://exttests');
-
-$CFG->behat_wwwroot   = 'http://webserver';
-$CFG->behat_dataroot  = '/var/www/behatdata';
-$CFG->behat_prefix = 'b_';
-$CFG->behat_profiles = array(
-    'default' => array(
-        'browser' => getenv('MOODLE_DOCKER_BROWSER'),
-        'wd_host' => 'http://selenium:4444/wd/hub',
-    ),
-);
-$CFG->behat_faildump_path = '/var/www/behatfaildumps';
-
-define('PHPUNIT_LONGTEST', true);
-
-if (getenv('MOODLE_DOCKER_PHPUNIT_EXTRAS')) {
-    define('TEST_SEARCH_SOLR_HOSTNAME', 'solr');
-    define('TEST_SEARCH_SOLR_INDEXNAME', 'test');
-    define('TEST_SEARCH_SOLR_PORT', 8983);
-
-    define('TEST_SESSION_REDIS_HOST', 'redis');
-    define('TEST_CACHESTORE_REDIS_TESTSERVERS', 'redis');
-
-    define('TEST_CACHESTORE_MONGODB_TESTSERVER', 'mongodb://mongo:27017');
-
-    define('TEST_CACHESTORE_MEMCACHED_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
-    define('TEST_CACHESTORE_MEMCACHE_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
-
-    define('TEST_LDAPLIB_HOST_URL', 'ldap://ldap');
-    define('TEST_LDAPLIB_BIND_DN', 'cn=admin,dc=openstack,dc=org');
-    define('TEST_LDAPLIB_BIND_PW', 'password');
-    define('TEST_LDAPLIB_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-
-    define('TEST_AUTH_LDAP_HOST_URL', 'ldap://ldap');
-    define('TEST_AUTH_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
-    define('TEST_AUTH_LDAP_BIND_PW', 'password');
-    define('TEST_AUTH_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-
-    define('TEST_ENROL_LDAP_HOST_URL', 'ldap://ldap');
-    define('TEST_ENROL_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
-    define('TEST_ENROL_LDAP_BIND_PW', 'password');
-    define('TEST_ENROL_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-}
 
 require_once(__DIR__ . '/lib/setup.php');
