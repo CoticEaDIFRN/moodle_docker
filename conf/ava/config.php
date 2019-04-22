d968af11f4f9f0be29daaff69ee92c08d271c442
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-04-22 12:43:22 -0300

    [ADD] activate debug using envs

--- conf/ava/config.php
+++ conf/ava/config.php
@@ -17,19 +17,32 @@ $CFG->dbpass    = _env('MOODLE_DOCKER_DBPASS', 'ava_pass');
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = ['dbcollation' => _env('MOODLE_DOCKER_DBCOLLATION')];
 
-$CFG->wwwroot   = _env('MOODLE_DOCKER_AVA_HOST', 'http://localhost/ava');
+$CFG->wwwroot   = _env('MOODLE_DOCKER_AVA_HOST', 'http://localhost:8036/ava');
 $CFG->dataroot  = '/var/moodledata';
 $CFG->admin     = 'admin';
 $CFG->directorypermissions = 0777;
 $CFG->smtphosts = 'mailhog:1025';
 
+$debug = _env('MOODLE_DEBUG', 'true') == 'true';
+
 // Debug options - possible to be controlled by flag in future..
-// $CFG->debug = (E_ALL | E_STRICT); // DEBUG_DEVELOPER
-// $CFG->debugdisplay = 1;
-// $CFG->debugstringids = 1; // Add strings=1 to url to get string ids.
-// $CFG->perfdebug = 15;
-// $CFG->debugpageinfo = 1;
-// $CFG->allowthemechangeonurl = 1;
-// $CFG->passwordpolicy = 0;
+if ($debug) {
+    $CFG->debug = (E_ALL | E_STRICT); // DEBUG_DEVELOPER
+    $CFG->debugdisplay = 1;
+    $CFG->debugstringids = 1; // Add strings=1 to url to get string ids.
+    $CFG->perfdebug = 15;
+    $CFG->debugpageinfo = 1;
+    $CFG->allowthemechangeonurl = 1;
+    $CFG->passwordpolicy = 0;
+} else {
+    // $CFG->debug = (E_ALL | E_STRICT); // DEBUG_DEVELOPER
+    $CFG->debug = (E_STRICT); // DEBUG_DEVELOPER
+    $CFG->debugdisplay = 0;
+    // $CFG->debugstringids = 1; // Add strings=1 to url to get string ids.
+    // $CFG->perfdebug = 15;
+    // $CFG->debugpageinfo = 1;
+    // $CFG->allowthemechangeonurl = 1;
+    // $CFG->passwordpolicy = 0;
+}
 
 require_once(__DIR__ . '/lib/setup.php');
